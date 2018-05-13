48ad7725371e3d3ba12d4242c2aaecb503e50e88
cnattras <saccharomyces.cerevisae@gmail.com>
2018-05-13 17:51:55 -0400

    adding script to download all journal club papers

---
+++ HowTos/DownloadSummer2018JournalClubPapers.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+wget  --user-agent="Mozilla" https://arxiv.org/pdf/nucl-ex/0603003
+mv 0603003 1JC0603003.pdf
+wget  --user-agent="Mozilla" https://arxiv.org/pdf/1509.07334
+mv 1509.07334 2JC1509.07334.pdf
+wget  --user-agent="Mozilla" https://arxiv.org/pdf/1705.01974
+mv 1705.01974 3JC1705.01974.pdf
+wget  --user-agent="Mozilla" https://arxiv.org/pdf/1509.06727
+mv 1509.06727 4JC1509.06727.pdf
+wget  --user-agent="Mozilla" https://arxiv.org/pdf/1208.4537
+mv 1208.4537 5JC1208.4537.pdf
+wget  --user-agent="Mozilla" https://arxiv.org/pdf/1406.0932
+mv 1406.0932 6JC1406.0932.pdf
+wget  --user-agent="Mozilla" https://arxiv.org/pdf/1609.05383
+mv 1609.05383 7JC1609.05383.pdf
+wget  --user-agent="Mozilla" https://arxiv.org/pdf/1509.07334
+mv 1509.07334 8JC1509.07334.pdf
+wget  --user-agent="Mozilla" https://arxiv.org/pdf/1801.03477
+mv 1801.03477 9JC1801.03477.pdf
+wget  --user-agent="Mozilla" https://arxiv.org/pdf/1411.2357
+mv 1411.2357 10JC1411.2357.pdf
+wget  --user-agent="Mozilla" https://arxiv.org/pdf/1311.0633
+mv 1311.0633 11JC1311.0633.pdf
+
+wget  --user-agent="Mozilla" https://arxiv.org/pdf/1802.04801
+mv 1802.04801 NewReview1802.04801.pdf
+
+
+
+wget https://cds.cern.ch/record/1558644/files/DP2013_018.pdf
+mv DP2013_018.pdf CMSBackgroundMethodDP2013_018.pdf
+wget  --user-agent="Mozilla" https://arxiv.org/pdf/1201.2423
+mv 1201.2423 ALICEBackgroundMethod1201.2423.pdf
